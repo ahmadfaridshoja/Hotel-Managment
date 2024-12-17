818683d1dbb2f915a509c98177b75d2386b565e5
Ahmad Farid Shoja <ahmadfaridshoja@gmial.com>
2024-12-17 05:03:50 -0500

    commit

---
+++ src/test/java/com/example/Hotel/Managment/HotelManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hotel.Managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
